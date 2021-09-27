298fb0bf93c7731ccd75d7db78347196d9ebc7df
Marcos Oliveira <pg.marcosoliveira@icloud.com>
2021-09-27 19:11:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("First Calculator Project with C#");
+            
+            //asking for a number and converting it to a double
+            Console.Write("Please enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
         }
     }
 }
